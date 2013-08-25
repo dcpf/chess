34522ab6b62b50167824d2272790e21ddde1805c
dcpf <dpf@theworld.com>
2013-08-25 07:22:18 -0400

    first real working version

--- js/util/test.js
+++ js/util/test.js
@@ -17,88 +17,103 @@ function movePiece ($obj, moveUp, moveRight, moveDown, moveLeft, targetOffset) {
           keepMoving = true;
      }
      if (keepMoving) {
-          setTimeout(function(){movePiece($obj, moveUp, moveRight, moveDown, moveLeft, targetOffset)}, 10);
+          setTimeout(function(){movePiece($obj, moveUp, moveRight, moveDown, moveLeft, targetOffset)}, 5);
      }
 }
 
 function doIt () {
-var $img = $('#sq60').children('img');
-var origOffset = $img.offset();
-$('#sq60').html('');
-$('body').append($img);
-$img.offset(origOffset);
-var $target = $('#sq67');
-var targetOffset = $target.offset();
-targetOffset.top += 4;
-targetOffset.left += 4;
-//$img.offset(targetOffset);
-var goUp = (origOffset.top > targetOffset.top);
-var goDown = (origOffset.top < targetOffset.top);
-var goLeft = (origOffset.left > targetOffset.left);
-var goRight = (origOffset.left < targetOffset.left);
 
-var moveUp = 0, moveDown = 0, moveLeft = 0, moveRight = 0;
-if (goUp && goRight) {
-     var distanceUp = origOffset.top - targetOffset.top;
-     var distanceRight = targetOffset.left - origOffset.left;
-     if (distanceUp > distanceRight) {
-          moveUp = distanceUp/distanceRight;
-          moveRight = 1;
-     } else if (distanceUp < distanceRight) {
-          moveRight = distanceRight/distanceUp;
-          moveUp = 1;
+     // get the piece and its orig offset
+     var $img = $('#sq60').children('img');
+     var origOffset = $img.offset();
+
+     // blank out the square where it lives
+     $('#sq60').html('');
+
+     // Get a handle on the target square. If there is a piece there, get its offset. Else, put the piece we already have there, and get its new offset.
+     var targetOffset;
+     var $target = $('#sq57');
+     var $img2 = $target.children('img');
+     if ($img2[0]) {
+          targetOffset = $img2.offset();
      } else {
+          $target.html($img);
+          targetOffset = $img.offset();
+          // blank out the target square
+          $target.html('');
+     }
+
+     // attach the img to the body and set its offset to the original offset
+     $('body').append($img);
+     $img.offset(origOffset);
+
+     var goUp = (origOffset.top > targetOffset.top);
+     var goDown = (origOffset.top < targetOffset.top);
+     var goLeft = (origOffset.left > targetOffset.left);
+     var goRight = (origOffset.left < targetOffset.left);
+
+     var moveUp = 0, moveDown = 0, moveLeft = 0, moveRight = 0;
+     if (goUp && goRight) {
+          var distanceUp = origOffset.top - targetOffset.top;
+          var distanceRight = targetOffset.left - origOffset.left;
+          if (distanceUp > distanceRight) {
+               moveUp = 1;
+               moveRight = 1/(distanceUp/distanceRight);
+          } else if (distanceUp < distanceRight) {
+               moveRight = 1;
+               moveUp = 1/(distanceRight/distanceUp);
+          } else {
+               moveUp = 1;
+               moveRight = 1;
+          }
+     } else if (goUp && goLeft) {
+          var distanceUp = origOffset.top - targetOffset.top;
+          var distanceLeft = origOffset.left - targetOffset.left;
+          if (distanceUp > distanceLeft) {
+               moveUp = 1;
+               moveLeft = 1/(distanceUp/distanceLeft);
+          } else if (distanceUp < distanceLeft) {
+               moveLeft = 1;
+               moveUp = 1/(distanceLeft/distanceUp);
+          } else {
+               moveUp = 1;
+               moveLeft = 1;
+          }
+     } else if (goDown && goRight) {
+          var distanceDown = targetOffset.top - origOffset.top;
+          var distanceRight = targetOffset.left - origOffset.left;
+          if (distanceDown > distanceRight) {
+               moveDown = 1;
+               moveRight = 1/(distanceDown/distanceRight);
+          } else if (distanceDown < distanceRight) {
+               moveRight = 1;
+               moveDown = 1/(distanceRight/distanceDown);
+          } else {
+               moveDown = 1;
+               moveRight = 1;
+          }
+     } else if (goDown && goLeft) {
+          var distanceDown = targetOffset.top - origOffset.top;
+          var distanceLeft = origOffset.left - targetOffset.left;
+          if (distanceDown > distanceLeft) {
+               moveDown = 1;
+               moveLeft = 1/(distanceDown/distanceRight);
+          } else if (distanceDown < distanceLeft) {
+               moveLeft = 1;
+               moveDown = 1/(distanceLeft/distanceDown);
+          } else {
+               moveDown = 1;
+               moveLeft = 1;
+          }
+     } else if (goUp) {
           moveUp = 1;
+     } else if (goDown) {
+          moveDown = 1;
+     } else if (goRight) {
           moveRight = 1;
-     }
-} else if (goUp && goLeft) {
-     var distanceUp = origOffset.top - targetOffset.top;
-     var distanceLeft = origOffset.left - targetOffset.left;
-     if (distanceUp > distanceLeft) {
-          moveUp = distanceUp/distanceLeft;
-          moveLeft = 1;
-     } else if (distanceUp < distanceLeft) {
-          moveLeft = distanceLeft/distanceUp;
-          moveUp = 1;
-     } else {
-          moveUp = 1;
+     } else if (goLeft) {
           moveLeft = 1;
      }
-} else if (goDown && goRight) {
-     var distanceDown = targetOffset.top - origOffset.top;
-     var distanceRight = targetOffset.left - origOffset.left;
-     if (distanceDown > distanceRight) {
-          moveDown = distanceDown/distanceRight;
-          moveRight = 1;
-     } else if (distanceDown < distanceRight) {
-          moveRight = distanceRight/distanceDown;
-          moveDown = 1;
-     } else {
-          moveDown = 1;
-          moveRight = 1;
-     }
-} else if (goDown && goLeft) {
-     var distanceDown = targetOffset.top - origOffset.top;
-     var distanceLeft = origOffset.left - targetOffset.left;
-     if (distanceDown > distanceLeft) {
-          moveDown = distanceDown/distanceRight;
-          moveLeft = 1;
-     } else if (distanceDown < distanceLeft) {
-          moveLeft = distanceLeft/distanceDown;
-          moveDown = 1;
-     } else {
-          moveDown = 1;
-          moveLeft = 1;
-     }
-} else if (goUp) {
-     moveUp = 1;
-} else if (goDown) {
-     moveDown = 1;
-} else if (goRight) {
-     moveRight = 1;
-} else if (goLeft) {
-     moveLeft = 1;
-} 
-console.log('moveUp: ' + moveUp + '; moveRight: ' + moveRight + '; moveDown: ' + moveDown + '; moveLeft: ' + moveLeft);
-movePiece($img, moveUp, moveRight, moveDown, moveLeft, targetOffset)
+     console.log('moveUp: ' + moveUp + '; moveRight: ' + moveRight + '; moveDown: ' + moveDown + '; moveLeft: ' + moveLeft);
+     movePiece($img, moveUp, moveRight, moveDown, moveLeft, targetOffset)
 }
