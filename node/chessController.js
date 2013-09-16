6bee50bf7770551a97233530544e45d7d831024f
dcpf <dpf@theworld.com>
2013-09-16 18:02:24 -0400

    added some minimal code to the controller

--- node/chessController.js
+++ node/chessController.js
@@ -1,4 +1,26 @@
 exports.enterGame = function (postData) {
+
+	var newOrExisting = postData['new_or_existing'];
+	if (newOrExisting === 'N') {
+		var player1Email = postData['Player1 Email'];
+		var player2Email = postData['Player2 Email'];
+		var newGameName = postData['New Game Name'];
+		createGame(player1Email, player2Email, newGameName);
+	} else {
+		var existingGameName = postData['Existing Game Name'];
+		var key = postData['key'];
+		enterExistingGame(existingGameName, key);
+	}
+
 	console.log(postData);
-	return 'index.html';
+	return 'chess.html';
+
 };
+
+function createGame (player1Email, player2Email, newGameName) {
+
+}
+
+function enterExistingGame (existingGameName, key) {
+
+}
