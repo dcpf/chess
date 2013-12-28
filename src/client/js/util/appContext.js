eab76e6fbe91f57601c1e3d16683421ceb72dbbe
dpf <dpf@theworld.com>
2013-12-28 09:52:07 -0500

    AppContext improvements

--- src/client/js/util/appContext.js
+++ src/client/js/util/appContext.js
@@ -6,72 +6,73 @@ var chess = chess || {};
 
 chess.AppContext = {
 
-    initialized: false,
+    _appContext: null,
 
     /**
     * Instantiates all of the objects and their dependencies needed for the chess game
     */
-    initializeGame: function () {
+    getAppContext: function () {
 
-        // If already initialized, just return this object
-        if (this.initialized) {
-            return this;
+        // If already initialized, just return the object
+        if (this._appContext !== null) {
+            return this._appContext;
         }
 
-        this.eventHandler = new chess.EventHandler();
-        this.notationConverter = new chess.NotationConverter();
-        this.genericDialogView = new chess.GenericDialogView({
-            eventHandler: this.eventHandler
+        var appContext = {};
+
+        appContext.eventHandler = new chess.EventHandler();
+        appContext.notationConverter = new chess.NotationConverter();
+        appContext.genericDialogView = new chess.GenericDialogView({
+            eventHandler: appContext.eventHandler
         });
-        this.board = new chess.Board({
-            eventHandler: this.eventHandler
+        appContext.board = new chess.Board({
+            eventHandler: appContext.eventHandler
         });
-        this.capturedPieces = new chess.CapturedPieces();
-        this.moveHistory = new chess.MoveHistory();
-        this.boardView = new chess.BoardView({
-            board: this.board,
-            eventHandler: this.eventHandler,
-            capturedPieces: this.capturedPieces,
-            moveHistory: this.moveHistory,
-            notationConverter: this.notationConverter
+        appContext.capturedPieces = new chess.CapturedPieces();
+        appContext.moveHistory = new chess.MoveHistory();
+        appContext.boardView = new chess.BoardView({
+            board: appContext.board,
+            eventHandler: appContext.eventHandler,
+            capturedPieces: appContext.capturedPieces,
+            moveHistory: appContext.moveHistory,
+            notationConverter: appContext.notationConverter
         });
-        this.boardSnapshotView = new chess.BoardSnapshotView({
-            eventHandler: this.eventHandler,
-            moveHistory: this.moveHistory,
-            notationConverter: this.notationConverter
+        appContext.boardSnapshotView = new chess.BoardSnapshotView({
+            eventHandler: appContext.eventHandler,
+            moveHistory: appContext.moveHistory,
+            notationConverter: appContext.notationConverter
         });
-        this.confirmMoveDialogView = new chess.ConfirmMoveDialogView({
-            eventHandler: this.eventHandler
+        appContext.confirmMoveDialogView = new chess.ConfirmMoveDialogView({
+            eventHandler: appContext.eventHandler
         });
-        this.playGameView = new chess.PlayGameView();
-        this.gameManager = new chess.GameManager({
-            eventHandler: this.eventHandler,
-            notationConverter: this.notationConverter,
-            board: this.board,
-            boardView: this.boardView,
-            playGameView: this.playGameView
+        appContext.playGameView = new chess.PlayGameView();
+        appContext.gameManager = new chess.GameManager({
+            eventHandler: appContext.eventHandler,
+            notationConverter: appContext.notationConverter,
+            board: appContext.board,
+            boardView: appContext.boardView,
+            playGameView: appContext.playGameView
         });
-        this.enterGameView = new chess.EnterGameView({
-            eventHandler: this.eventHandler,
-            gameManager: this.gameManager
+        appContext.enterGameView = new chess.EnterGameView({
+            eventHandler: appContext.eventHandler,
+            gameManager: appContext.gameManager
         });
 
         // These views just need to be instantiated - no need to assign to a variable
         new chess.CapturedPiecesView({
-            capturedPieces: this.capturedPieces
+            capturedPieces: appContext.capturedPieces
         });
         new chess.MoveHistoryView({
-            moveHistory: this.moveHistory,
-            eventHandler: this.eventHandler
+            moveHistory: appContext.moveHistory,
+            eventHandler: appContext.eventHandler
         });
         new chess.MessagesView({
-            eventHandler: this.eventHandler,
-            board: this.board
+            eventHandler: appContext.eventHandler,
+            board: appContext.board
         });
 
-        this.initialized = true;
-
-        return this;
+        this._appContext = appContext;
+        return this._appContext;
         
     }
 
