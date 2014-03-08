64caf0fc782db1d61dae3bf5b1401348b7ddcb59
dpf <dpf@theworld.com>
2014-03-07 21:55:11 -0500

    obfuscating gameID

--- src/server/dao/mongoGameDAO.js
+++ src/server/dao/mongoGameDAO.js
@@ -15,7 +15,7 @@ var getGameObject = function (gameID) {
         id;
     
     try {
-        id = mongojs.ObjectId(gameID);
+        id = mongojs.ObjectId(_deobfuscateGameID(gameID));
     } catch (err) {
         // If ObjectId() throws an error, it means the gameID is invalid
         deferred.reject(new Error('Invalid Game ID: ' + gameID));
@@ -38,14 +38,14 @@ var getGameObject = function (gameID) {
 
 var saveGame = function (gameID, gameObj) {
     var deferred = q.defer();
-    var id = (gameID) ? mongojs.ObjectId(gameID) : null;
+    var id = (gameID) ? mongojs.ObjectId(_deobfuscateGameID(gameID)) : null;
 	db.games.save({_id: id, gameObj: gameObj}, function (err, savedObj) {
         if (err) {
             deferred.reject(err);
         } else if (!savedObj) {
             deferred.reject(new Error('Game ' + gameID + ' not saved'));
         } else {
-            gameID = gameID || savedObj._id;
+            gameID = gameID || _obfuscateGameID(savedObj._id.toHexString());
             deferred.resolve(gameID);
         }
     });
@@ -59,3 +59,27 @@ var createGame = function (gameObj) {
 exports.getGameObject = getGameObject;
 exports.saveGame = saveGame;
 exports.createGame = createGame;
+
+/**
+* Obfuscate the gameID by replacing all 0s at the end of the ID with a single hyphen.
+* 5313a204d2cfc5da53000001 > 5313a204d2cfc5da53-1
+* 5313a204d2cfc5da53000017 > 5313a204d2cfc5da53-17
+*/
+function _obfuscateGameID (gameID) {
+    return gameID.replace(/0+([^0]+)$/, '-$1');
+}
+
+/**
+* De-obfuscate the gameID by replacing the hyphen with the correct number of 0s.
+* 5313a204d2cfc5da53-1 > 5313a204d2cfc5da53000001
+* 5313a204d2cfc5da53-17 > 5313a204d2cfc5da53000017
+*/
+function _deobfuscateGameID (gameID) {
+    var zeros = '',
+        parts = gameID.split('-'),
+        numZeros = 25 - gameID.length;
+    while (zeros.length != numZeros) {
+        zeros += '0';
+    }
+    return parts[0] + zeros + parts[1];
+}
