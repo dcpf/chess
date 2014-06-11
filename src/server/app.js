03fc91917fde4ab1adaf307d1bcd8e4fbedc9747
dpf <dpf@theworld.com>
2014-06-11 19:52:15 -0400

    upgraded to express 4.4.2

--- src/server/app.js
+++ src/server/app.js
@@ -9,7 +9,16 @@ var appUrl = require('./model/appUrl');
 // before doing anything more, initialize the configuration
 initConfig();
 
+// express and middleware
 var express = require('express');
+var morgan  = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var compression = require('compression');
+var errorHandler = require('errorhandler');
+
 var routes = require('./routes');
 var http = require('http');
 
@@ -21,31 +30,29 @@ app.set('views', path.join(__dirname, 'view'));
 app.engine('html', require('uinexpress').__express);
 app.set('view engine', 'html');
 
-//app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.compress());
-// ../../webapp is where grunt copies the static files to
-app.use('/webapp', express.static(path.join(__dirname, '../../webapp'), { maxAge: 31536000000 }));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.use(morgan());
+app.use(bodyParser());
+app.use(bodyParser.urlencoded());
+app.use(methodOverride());
+app.use(cookieParser());
+app.use(session({secret: 'jellyJam'}));
+app.use(compression());
 
 // routes
-
 app.get('/', routes.index);
 app.post('/createGame', routes.createGame);
 app.post('/enterGame', routes.enterGame);
 app.post('/saveMove', routes.saveMove);
 app.post('/updateUserPrefs', routes.updateUserPrefs);
 app.post('/logClientError', routes.logClientError);
+
+// ../../webapp is where grunt copies the static files to
+app.use('/webapp', express.static(path.join(__dirname, '../../webapp'), { maxAge: 31536000000 }));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
 
 http.createServer(app).listen(GLOBAL.APP_URL.port);
 
