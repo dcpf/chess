45f1bf2e71f20677dc2ebac984df34a4b1a6f23b
dpf@theworld.com <dpf@theworld.com>
2014-07-25 16:46:45 -0400

    code in gameStarter.js does not need to be in a self-calling function

--- src/client/js/util/gameStarter.js
+++ src/client/js/util/gameStarter.js
@@ -2,17 +2,13 @@
 * Copyright (c) 2000 - 2014 dpf, dpf@theworld.com
 */
 
-(function(){
+// Instantiate the appContext
+var appContext = AppContext.getAppContext();
 
-  // Instantiate the appContext
-  var appContext = AppContext.getAppContext();
-
-  // If the page has loaded with a gameID, start the game. This will occur when the gameID URL param is passed in: ?gameID=531d080566b22a611f-1-82197
-  // Else, show the enterGame view.
-  if (chessAttrs.vars.gameID && !chessAttrs.vars.error) {
-    appContext.gameManager.startGame();
-  } else {
-    appContext.enterGameView.show(chessAttrs.vars.error);
-  }
-
-})();
+// If the page has loaded with a gameID, start the game. This will occur when the gameID URL param is passed in: ?gameID=531d080566b22a611f-1-82197
+// Else, show the enterGame view.
+if (chessAttrs.vars.gameID && !chessAttrs.vars.error) {
+  appContext.gameManager.startGame();
+} else {
+  appContext.enterGameView.show(chessAttrs.vars.error);
+}
