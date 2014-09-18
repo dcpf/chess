8b0623b4a2e0f502bc2ac078f7c42f0c7d848d50
dpf@theworld.com <dpf@theworld.com>
2014-09-18 06:59:19 -0400

    missed files from prev commit

---
+++ src/client/admin/components/find-games-by-email/tag.js
@@ -0,0 +1,32 @@
+(function(){
+    
+Polymer({
+    
+    ready: function() {
+        this.response = '';
+        this.errMsg = '';
+    },
+    
+    submit: function(event, detail, sender) {
+        if (event.keyCode === 13) {
+            var xhr = this.$.xhr;
+            xhr.params = {"email": this.$.email.value};
+            xhr.go();
+        }
+    },
+    
+    handleSuccess: function (event, res) {
+        this.$.errMsg.style.display = 'none';
+        this.$.response.style.display = 'block';
+        this.response = JSON.stringify(res.response);
+    },
+    
+    handleError: function (event, res) {
+        this.$.response.style.display = 'none';
+        this.errMsg = res.xhr.responseText;
+        this.$.errMsg.style.display = 'block';
+    }
+    
+});
+    
+})();
