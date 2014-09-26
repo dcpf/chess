fb8e8d2675334539a9f00fdfdb7188006cd4bdbe
dpf@theworld.com <dpf@theworld.com>
2014-09-26 16:26:38 -0400

    comment update

--- src/client/js/util/gameStarter.js
+++ src/client/js/util/gameStarter.js
@@ -5,8 +5,15 @@
 // Instantiate the appContext
 var appContext = AppContext.getAppContext();
 
-// If the page has loaded with a gameID, start the game. This will occur when the gameID URL param is passed in: ?gameID=531d080566b22a611f-1-82197
-// Else, show the enterGame view.
+/*
+ If the page has loaded with a gameID, start the game. This will occur when the gameID is passed in as either:
+ 
+ - /play/531d080566b22a611f1-82197
+ or
+ - ?gameID=531d080566b22a611f1-82197
+
+ Else, show the enterGame view.
+*/
 if (chessAttrs.vars.gameID && !chessAttrs.vars.error) {
   appContext.gameManager.startGame();
 } else {
